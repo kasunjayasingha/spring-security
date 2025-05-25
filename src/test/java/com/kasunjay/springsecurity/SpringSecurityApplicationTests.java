9f1c57079c0cdd0cb91df6b353edfc32408ba683
Kasun Jayasinghe <kasun_t@epiclanka.net>
2025-05-25 15:55:51 +0530

    Spring security project initiate.

---
+++ src/test/java/com/kasunjay/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kasunjay.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
